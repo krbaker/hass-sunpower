d1f349a0a27840a0711981e5b4adc92fd5dad3cf
Keith Baker <syzygy@specialks.com>
2020-10-18 20:20:37 -0400

    use the right keys to for Interters and Meters

--- custom_components/sunpower/binary_sensor.py
+++ custom_components/sunpower/binary_sensor.py
@@ -101,7 +101,7 @@ class SunPowerMeterState(SunPowerMeterEntity):
     @property
     def state(self):
         """Get the current value"""
-        return self.coordinator.data[PVS_DEVICE_TYPE][self.base_unique_id][METER_STATE]
+        return self.coordinator.data[METER_DEVICE_TYPE][self.base_unique_id][METER_STATE]
 
     @property
     def is_on(self):
@@ -130,7 +130,7 @@ class SunPowerInverterState(SunPowerInverterEntity):
     @property
     def state(self):
         """Get the current value"""
-        return self.coordinator.data[PVS_DEVICE_TYPE][self.base_unique_id][INVERTER_STATE]
+        return self.coordinator.data[INVERTER_DEVICE_TYPE][self.base_unique_id][INVERTER_STATE]
 
     @property
     def is_on(self):
