c22a6b7f1e1d0e2873b6bfe5f9ffcd1f96c4de34
Chistopher Kochan <5183896+crkochan@users.noreply.github.com>
2021-09-05 23:49:22 -0700

    Correct case on SunPower name.

--- custom_components/sunpower/__init__.py
+++ custom_components/sunpower/__init__.py
@@ -78,7 +78,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = DataUpdateCoordinator(
         hass,
         _LOGGER,
-        name="Sunpower PVS",
+        name="SunPower PVS",
         update_method=async_update_data,
         update_interval=timedelta(seconds=UPDATE_INTERVAL),
     )
