84fc05aeb54fdf0b6caf278bcd74cd93408c62bf
Keith Baker <syzygy@specialks.com>
2024-04-11 16:04:58 -0400

    revert quote change

--- custom_components/sunpower/entity.py
+++ custom_components/sunpower/entity.py
@@ -4,6 +4,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 
+
 class SunPowerEntity(CoordinatorEntity):
     def __init__(self, coordinator, my_info, parent_info):
         """Initialize the sensor."""
@@ -11,18 +12,24 @@ class SunPowerEntity(CoordinatorEntity):
         self._my_info = my_info
         self._parent_info = parent_info
         self.base_unique_id = self._my_info.get("SERIAL", "")
-    
+
     @property
     def device_info(self):
+        serial = self._my_info.get("SERIAL", "UnknownSerial")
+        model = self._my_info.get("MODEL", "UnknownModel")
+        name = self._my_info.get("DESCR", f"{model} {serial}")
         device_info = {
             "identifiers": {(DOMAIN, self.base_unique_id)},
-            "name": f"{self._my_info.get('DESCR', f"{self._my_info.get('MODEL', 'UnknownModel')} {self._my_info.get('SERIAL', 'UnknownSerial')}")}",
+            "name": name,
             "manufacturer": "SunPower",
-            "model": f"{self._my_info.get('MODEL', 'UnknownModel')}",
+            "model": model,
             "sw_version": f"{self._my_info.get('SWVER', 'UnknownVersion')}",
         }
         if self._parent_info is not None:
-            device_info["via_device"] = f"{self._parent_info.get('SERIAL', 'UnknownParent')}"
+            device_info["via_device"] = (
+                DOMAIN,
+                f"{self._parent_info.get('SERIAL', 'UnknownParent')}",
+            )
         return device_info
 
 
@@ -40,11 +47,15 @@ class SunPowerPVSEntity(CoordinatorEntity):
         """Sunpower PVS device info."""
         device_info = {
             "identifiers": {(DOMAIN, self.base_unique_id)},
-            "name": "{} {}".format(self._pvs_info["MODEL"], self._pvs_info["SERIAL"]),
+            "name": "{} {}".format(
+                self._pvs_info["MODEL"],
+                self._pvs_info["SERIAL"],
+            ),
             "manufacturer": "SunPower",
             "model": self._pvs_info["MODEL"],
             "sw_version": "{}, Hardware: {}".format(
-                self._pvs_info["SWVER"], self._pvs_info["HWVER"]
+                self._pvs_info["SWVER"],
+                self._pvs_info["HWVER"],
             ),
         }
         return device_info
