20d6b5784834a83f950161a92f3a8d1106fc9f0a
Keith Baker <syzygy@specialks.com>
2024-04-16 19:31:24 -0400

    Remove unused classes but pull in some of the reasons they were different (collecting some special fields)

--- custom_components/sunpower/entity.py
+++ custom_components/sunpower/entity.py
@@ -18,92 +18,18 @@ class SunPowerEntity(CoordinatorEntity):
         serial = self._my_info.get("SERIAL", "UnknownSerial")
         model = self._my_info.get("MODEL", "UnknownModel")
         name = self._my_info.get("DESCR", f"{model} {serial}")
+        hw_version = self._my_info.get("HWVER", self._my_info.get("hw_version", "Unknown"))
+        sw_version = f"{self._my_info.get("SWVER", "Unknown")} Hardware: {hw_version}"
         device_info = {
             "identifiers": {(DOMAIN, self.base_unique_id)},
             "name": name,
             "manufacturer": "SunPower",
             "model": model,
-            "sw_version": f"{self._my_info.get('SWVER', 'UnknownVersion')}",
+            "sw_version": sw_version
         }
         if self._parent_info is not None:
             device_info["via_device"] = (
                 DOMAIN,
                 f"{self._parent_info.get('SERIAL', 'UnknownParent')}",
             )
-        return device_info
-
-
-class SunPowerPVSEntity(CoordinatorEntity):
-    """Base class for sunpower pvs entities."""
-
-    def __init__(self, coordinator, pvs_info):
-        """Initialize the sensor."""
-        super().__init__(coordinator)
-        self.base_unique_id = pvs_info["SERIAL"]
-        self._pvs_info = pvs_info
-
-    @property
-    def device_info(self):
-        """Sunpower PVS device info."""
-        device_info = {
-            "identifiers": {(DOMAIN, self.base_unique_id)},
-            "name": "{} {}".format(
-                self._pvs_info["MODEL"],
-                self._pvs_info["SERIAL"],
-            ),
-            "manufacturer": "SunPower",
-            "model": self._pvs_info["MODEL"],
-            "sw_version": "{}, Hardware: {}".format(
-                self._pvs_info["SWVER"],
-                self._pvs_info["HWVER"],
-            ),
-        }
-        return device_info
-
-
-class SunPowerMeterEntity(CoordinatorEntity):
-    """Base class for sunpower meter entities."""
-
-    def __init__(self, coordinator, meter_info, pvs_info):
-        """Initialize the sensor."""
-        super().__init__(coordinator)
-        self.base_unique_id = meter_info["SERIAL"]
-        self._pvs_info = pvs_info
-        self._meter_info = meter_info
-
-    @property
-    def device_info(self):
-        """Sunpower Inverter device info."""
-        device_info = {
-            "identifiers": {(DOMAIN, self.base_unique_id)},
-            "name": self._meter_info["DESCR"],
-            "manufacturer": "SunPower",
-            "model": self._meter_info["MODEL"],
-            "sw_version": self._meter_info["SWVER"],
-            "via_device": (DOMAIN, self._pvs_info["SERIAL"]),
-        }
-        return device_info
-
-
-class SunPowerInverterEntity(CoordinatorEntity):
-    """Base class for sunpower inverter entities."""
-
-    def __init__(self, coordinator, inverter_info, pvs_info):
-        """Initialize the sensor."""
-        super().__init__(coordinator)
-        self.base_unique_id = inverter_info["SERIAL"]
-        self._pvs_info = pvs_info
-        self._inverter_info = inverter_info
-
-    @property
-    def device_info(self):
-        """Sunpower Inverter device info."""
-        device_info = {
-            "identifiers": {(DOMAIN, self.base_unique_id)},
-            "name": self._inverter_info["DESCR"],
-            "manufacturer": self._inverter_info["TYPE"],
-            "model": self._inverter_info["MODEL"],
-            "sw_version": self._inverter_info["SWVER"],
-            "via_device": (DOMAIN, self._pvs_info["SERIAL"]),
-        }
         return device_info
